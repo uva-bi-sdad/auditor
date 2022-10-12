690f42ec7a28106aef473546d1cdfc5732811d10
Alan <ahw9f@virginia.edu>
2022-10-12 12:08:43 -0400

    [#4] feat: recursive search for measure_info keys to adopt for the different measure_info versions and still create manifest.json

--- audit.py
+++ audit.py
@@ -1,26 +1,17 @@
 import os
-import hashlib
 import argparse
 import json
 import logging
 from pathlib import Path
-from difflib import SequenceMatcher
 import settings
 from datetime import datetime
+import util
+from pprint import pprint
 
 """
 Given the path to a directory, generate a manifest file
 [Finding files recursively] https://stackoverflow.com/questions/2186525/how-to-use-glob-to-find-files-recursively
 """
-
-
-def similar(a, b):
-    # Checking for string similarity
-    return SequenceMatcher(None, a, b).ratio()
-
-
-def get_md5(filepath):
-    return hashlib.md5(open(filepath, "rb").read()).hexdigest()
 
 
 def evaluate_folder(answer, dirpath):
@@ -33,36 +24,34 @@ def evaluate_folder(answer, dirpath):
     for path in Path(dirpath).rglob("distribution/**/*"):
         logging.debug("\tEvaluating: %s" % path.name)
 
-        if os.path.isfile(path):
-            parent_dir = path.parent
-            to_append = {
-                "name": path.name,
-                # Append the raw github location, add the repository name after, and then add the path under the repository
-                "path": os.path.relpath(path),
-                "md5": get_md5(path),
-                "size": os.path.getsize(path),
-            }
+        if not os.path.isfile(path):
+            # if path is not a file, skip to the next file to check
+            continue
+        parent_dir = path.parent
+        to_append = {
+            "name": path.name,
+            # Append the raw github location, add the repository name after, and then add the path under the repository
+            "path": os.path.relpath(path),
+            "md5": util.get_md5(path),
+            "size": os.path.getsize(path),
+        }
 
-            measure_data = None
-            # Check if there is a manifest file. If so, then try to append the measure info
-            logging.debug(
-                "[%s] checking %s in suffix list"
-                % (path.suffix in settings.SUFFIX_TO_MEASURE, path.suffix)
+        measure_data = None
+        # Check if there is a manifest file. If so, then try to append the measure info
+        if (
+            path.suffix in settings.SUFFIX_TO_MEASURE
+            and "measure_info.json" in os.listdir(parent_dir)
+        ):
+            measure_data = search_measure_info(
+                path, os.path.join(parent_dir, "measure_info.json"), answer
             )
-            if (
-                path.suffix in settings.SUFFIX_TO_MEASURE
-                and "measure_info.json" in os.listdir(parent_dir)
-            ):
-                measure_data = search_measure_info(
-                    path, os.path.join(parent_dir, "measure_info.json"), answer
-                )
-                if measure_data is not None:
-                    to_append["measure_info"] = measure_data
-                else:
-                    answer["measure_not_found"] += 1
-                logging.debug("-" * 80)
-            # regardless of whether a measure info is found, you should still append the md5
-            answer["data"].append(to_append)
+            if measure_data is not None:
+                to_append["measure_info"] = measure_data
+            else:
+                answer["measure_not_found"].append(str(path.resolve()))
+            logging.debug("-" * 80)
+        # regardless of whether a measure info is found, you should still append the md5
+        answer["data"].append(to_append)
 
 
 def search_measure_info(path, measure_info_path, answer):
@@ -75,19 +64,37 @@ def search_measure_info(path, measure_info_path, answer):
     if "_references" in measure_info:
         answer["_references"].update(measure_info["_references"])
 
-    logging.debug(measure_info)
     if len(measure_info) > 0:
-        keys = measure_info.keys()
-        logging.debug(path.name)
-        logging.debug(keys)
-        closest_matches = [
-            key for key in keys if key.split(":")[0] in path.name
-        ]  # the measure is ':' delimited, and the first element
+        # there are two different formats of measure_info files, so look recurisvely for the measure table instead
+        tables = util._finditems(measure_info, "measure_table")
+        matches = [table for table in tables if table == path.name.split(".")[0]]
 
-        if len(closest_matches) > 0:
-            logging.debug("Closest matches: %s" % closest_matches)
-            matched_keys = [measure_info[match] for match in closest_matches]
-            logging.debug("Matched keys found: %s" % matched_keys)
+        logging.info("searching: %s" % path.name.split(".")[0])
+        logging.info("\ttables: %s" % tables)
+        logging.info("\tmatches: %s" % matches)
+        logging.info("-" * 80)
+
+        if len(matches) > 0:
+            # search recursively in the same order and reconstruct the dictionaries to add
+            matched_keys = []
+
+            # a dictionary containing lists of parsed elements, assuming 1:1 between key and value
+            ad = {}
+            for k in util.MEASURE_KEYS:
+                ad[k] = util._finditems(measure_info, k)
+                # print("%s: %s" % (k, len(ad[k])))
+            # pprint(ad)
+            for i in range(len(tables)):
+                if not tables[i] == path.name.split(".")[0]:  # if there is a match
+                    continue
+                d = {}
+                for key in util.MEASURE_KEYS:
+                    if len(ad[key]) > i:  # because sometimes this value is empty
+                        d[key] = ad[key][i]
+                    else:
+                        d[key] = []
+                matched_keys.append(d)
+                logging.debug("Matched keys found: %s" % matched_keys)
             return matched_keys
         else:
             logging.debug("No matches found!")
@@ -101,7 +108,7 @@ def main(root, test=False):
     root = os.path.abspath(root)
     answer = {
         "name": os.path.basename(root),
-        "measure_not_found": 0,
+        "measure_not_found": [],
         "count": 0,
         "utc_audited": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
         "data": [],
@@ -122,7 +129,10 @@ def main(root, test=False):
         export_file = os.path.join(root, "manifest.json")
         with open(export_file, "w") as f:
             json.dump(answer, f, indent=2)
-        logging.info("[%s] Manifest file created", os.path.isfile(export_file))
+        logging.info(
+            "[%s] Manifest file created at: %s"
+            % (os.path.isfile(export_file), export_file),
+        )
 
 
 if __name__ == "__main__":
